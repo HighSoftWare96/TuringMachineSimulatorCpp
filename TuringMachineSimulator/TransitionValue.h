15e538fccae792c0e8d62114c21bbb8dae3aa938
Bertoncelli Giovanni <bertonc96@gmail.com>
2018-04-18 14:34:30 +0200

    Documentazione TransitionValue.h

--- TuringMachineSimulator/TransitionValue.h
+++ TuringMachineSimulator/TransitionValue.h
@@ -1,22 +1,35 @@
 #ifndef TRANSITION_VALUE
 #define TRANSITION_VALUE
 namespace mdtModels {
-
+	/// Enum per i movimenti della testina della MdT.
+	/** Questa enum utilizzata in questo namespace rappresenta 
+	i valori ammessi per i movimenti della testina ed ha associati 
+	gli interi corrispondenti da sommare al valore della posizione attuale
+	della testina. */
 	enum Movement {
 		L = -1,
 		N = 0,
 		R = 1
 	};
-
+	/// Classe per rappresentare i valori di una transizione.
+	/** Usata nella mappa delle transizioni per ricavare il prossimo stato della macchina, il simbolo da scrivere 
+	e il movimento da fare data una certa chiave. */
 	class TransitionValue
 	{
 	public:
+		/// Prossimo stato della MdT.
 		int nextState;
+		/// Prossimo simbolo della MdT.
 		char nextSymbol;
+		/// Prossimo movimento della MdT.
 		Movement nextMove;
 
+		/// Costruttore vuoto, usato per la std::map.
 		TransitionValue();
+		/// Costruttore che riceve tutte i dati da assegnare alle proprietà 
+		/// dell'oggetto stesso.
 		TransitionValue(int nextState, char nextSymbol, Movement nextMove);
+		/// Distruttore vuoto.
 		~TransitionValue();
 	};
 }
